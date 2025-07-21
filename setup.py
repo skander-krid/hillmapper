443c7c1778620973f0e0b5d5fcde5a27674b68ec
Salim <hertelli@in.tum.de>
2025-07-21 22:02:10 +0200

    it works

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from skbuild import setup
-from setuptools import find_packages
 
 setup(
     name="hillmapper",
     version="0.1.0",
-    packages=find_packages(where="."),
-    package_dir={"": "."},
+    packages=["hillmapper"],
+    package_dir={"hillmapper": "hillmapper"},
     include_package_data=True,
 )
