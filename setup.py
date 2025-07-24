019f7f1f5b0d0f1e46f146c26f63a6a5942fcf82
Salim <hertelli@in.tum.de>
2025-07-24 15:14:34 +0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="hillmapper",
-    version="0.1.0",
+    version="0.1.1",
     packages=["hillmapper"],
     package_dir={"hillmapper": "hillmapper"},
     include_package_data=True,
